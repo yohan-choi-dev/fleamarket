edf0bd4435d4c31e39a61cc9e3244bc59ea03f2e
William To <towilliam03@gmail.com>
2020-01-19 00:38:44 -0500

    Add ClickableLink component

---
+++ src/stories/2-ClickableLink.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ClickableLink from '../components/ClickableLink/ClickableLink';
+
+export default {
+  title: 'ClickableLink',
+  component: ClickableLink,
+};
+
+export const Normal = () => (
+  <ClickableLink>Log In</ClickableLink>
+);
