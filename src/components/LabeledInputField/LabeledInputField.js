6543104c8bbda3f0975ac3a4277b53915cb7c772
William To <towilliam03@gmail.com>
2020-02-05 18:09:01 -0500

    FML

---
+++ src/components/LabeledInputField/LabeledInputField.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import './LabeledInputField.css';
+
+function LabeledInputField(props) {
+  const { className, label, inputField } = props;
+  const { classNames, id, name, type, placeholder, required, autoFocus, style, onChangeHandler, value } = inputField;
+  return (
+    <div className={classnames('LabeledInputField', className)}>
+      <label className="LabeledInputField-label" htmlFor={id}>
+        {label}
+      </label>
+      <input
+        className={classnames('LabeledInputField-input', classNames)}
+        id={id} type={type} name={name} placeholder={placeholder}
+        required={required} autoFocus={autoFocus} style={style}
+        onChange={onChangeHandler}
+        value={value}
+      />
+    </div>
+  );
+}
+
+LabeledInputField.propTypes = {
+  className: PropTypes.string,
+  label: PropTypes.string,
+  inputField: PropTypes.shape({
+    classNames: PropTypes.string,
+    id: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    placeholder: PropTypes.string,
+    required: PropTypes.bool,
+    autoFocus: PropTypes.bool,
+    style: PropTypes.object,
+    onChangeHandler: PropTypes.func,
+    value: PropTypes.string
+  })
+}
+
+export default LabeledInputField;
