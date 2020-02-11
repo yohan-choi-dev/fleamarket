8308e9b788b99f1c088c15ccc036e7aa4096791d
wingtunglau <wtlau@mysenca.ca>
2020-02-11 13:24:41 -0500

    dropdown link

---
+++ src/stories/DropdownLink/DropdownLink.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DropdownLink from '../../components/DropdownLink/DropdownLink';
+
+export default {
+  title: 'DropdownLink',
+  component: DropdownLink
+};
+
+
+
+
+export const Normal = () => <DropdownLink username='wing tung lau'/>;
