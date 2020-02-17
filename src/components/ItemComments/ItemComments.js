ed648eb072e412c34315e994d31206cfeb1ae2bc
ahmad <59669358+Sky-dev781@users.noreply.github.com>
2020-02-17 12:56:02 -0500

    Add files via upload

--- src/components/ItemComments/ItemComments.js
+++ src/components/ItemComments/ItemComments.js
@@ -6,12 +6,12 @@ import './ItemComments.css';
   const { comments } = props;
   return (
     <div>
-     <div className="Itemcomments">
-      <div className="Itemcomments-comments-info">
-        <h1 className="Itemcomments-comments-picture">{comments.picture}</h1>
-        <h1 className="Itemcomments-comments-name">{comments.name}</h1>
-        <h2 className="Itemcomments-comments-rating">{comments.rating}</h2>
-        <p className="Itemcomments-comments-description">{comments.description}</p>
+     <div className="ItemComments">
+      <div className="ItemComments-comments-info">
+        <h1 className="ItemComments-comments-picture">{comments.picture}</h1>
+        <h1 className="ItemComments-comments-name">{comments.name}</h1>
+        <h2 className="ItemComments-comments-rating">{comments.rating}</h2>
+        <p className="ItemComments-comments-description">{comments.description}</p>
       </div>
       </div>
     </div>
