e17c24e3d653408c29effef98c93ddf1d3d98aad
Yohan Choi <ychoi63@myseneca.ca>
2020-02-18 05:08:36 -0500

    added cluster features to server.js

--- server.js
+++ server.js
@@ -1,90 +1,110 @@
-const express = require("express");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 
-const path = require("path");
-const fs = require("fs");
-const bodyParser = require("body-parser");
-const multer = require("multer");
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
 
-// import a database and models
-const sequelize = require("./utils/database");
-const { User, Token } = require("./models/user");
-const Item = require("./models/item");
-const Comment = require("./models/comment");
-const Category = require("./models/category");
-const Feedback = require("./models/feedback");
-const Message = require("./models/message");
-const Notification = require("./models/notification");
-const UserItemBridge = require("./models/user-item-bridge");
-
-const hostname = "10.102.112.129";
-const port = process.env.PORT | 10034;
-
-const authRoutes = require("./routes/auth");
-const itemRoutes = require("./routes/items");
-
-const ChatService = require("./service/chat-service");
-const app = express();
-
-const fileStorage = multer.diskStorage({
-    destination: (req, res, cb) => {
-        cb(null, "images");
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toISOString() + "-" + file.originalname);
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
     }
-});
 
-const fileFilter = (req, file, cb) => {
-    if (
-        file.mimetype == "image/png" ||
-        file.mimetype == "image/jpg" ||
-        file.mimetype == "image/jpeg"
-    ) {
-        cb(null, true);
-    }
-    {
-        cb(null, false);
-    }
-};
-
-app.use(bodyParser.json());
-
-// This middleware allows CORS
-app.use((req, res, next) => {
-
-    // This header allows the specific origin to access to the api
-    // res.setHeader('Access-Control-Allow-Origin', 'myvmlab.senecacollege.ca');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // This header aloows the spcific method to be used
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method == 'OPTIONS') {
-        res.status(200).send();
-    }
-    next();
-});
-
-app.use("/api/auth", authRoutes);
-app.use("/api/items", itemRoutes);
-
-app.use((error, req, res, next) => {
-    console.log(error);
-    // status 500 code: internal server error
-    const status = error.statusCode || 500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({ message: message, data: data });
-});
-
-
-
-sequelize
-    .sync()
-    .then(res => {
-        const server = app.listen(port, () => console.log("Server is running"));
-        const mailService = require("./service/mail-service").init();
-    })
-    .catch(err => {
-        console.log(err);
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
     });
+} else {
+    const express = require("express");
+
+    const path = require("path");
+    const fs = require("fs");
+    const bodyParser = require("body-parser");
+    const multer = require("multer");
+
+    // import a database and models
+    const sequelize = require("./utils/database");
+    const { User, Token } = require("./models/user");
+    const Item = require("./models/item");
+    const Comment = require("./models/comment");
+    const Category = require("./models/category");
+    const Feedback = require("./models/feedback");
+    const Message = require("./models/message");
+    const Notification = require("./models/notification");
+    const UserItemBridge = require("./models/user-item-bridge");
+
+    const port = process.env.PORT | 10034 | 3000;
+
+    const authRoutes = require("./routes/auth");
+    const itemRoutes = require("./routes/items");
+
+    const ChatService = require("./service/chat-service");
+    const app = express();
+
+    const fileStorage = multer.diskStorage({
+        destination: (req, res, cb) => {
+            cb(null, "images");
+        },
+        filename: (req, file, cb) => {
+            cb(null, new Date().toISOString() + "-" + file.originalname);
+        }
+    });
+
+    const fileFilter = (req, file, cb) => {
+        if (
+            file.mimetype == "image/png" ||
+            file.mimetype == "image/jpg" ||
+            file.mimetype == "image/jpeg"
+        ) {
+            cb(null, true);
+        }
+        {
+            cb(null, false);
+        }
+    };
+
+    app.use(bodyParser.json());
+
+    // This middleware allows CORS
+    app.use((req, res, next) => {
+        // This header allows the specific origin to access to the api
+        // res.setHeader('Access-Control-Allow-Origin', 'myvmlab.senecacollege.ca');
+        res.setHeader("Access-Control-Allow-Origin", "*");
+
+        // This header aloows the spcific method to be used
+        res.setHeader(
+            "Access-Control-Allow-Methods",
+            "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+        );
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "Content-Type, Authorization"
+        );
+        if (req.method == "OPTIONS") {
+            res.status(200).send();
+        }
+        next();
+    });
+
+    app.use("/api/auth", authRoutes);
+    app.use("/api/items", itemRoutes);
+
+    app.use((error, req, res, next) => {
+        console.log(error);
+        // status 500 code: internal server error
+        const status = error.statusCode || 500;
+        const message = error.message;
+        const data = error.data;
+        res.status(status).json({ message: message, data: data });
+    });
+
+    sequelize
+        .sync()
+        .then(res => {
+            const server = app.listen(port, () =>
+                console.log(`Worker ${process.pid} is running on ${port}`)
+            );
+            const mailService = require("./service/mail-service").init();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
