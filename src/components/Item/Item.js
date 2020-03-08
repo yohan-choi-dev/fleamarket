8c4cd04d7752755ddf8c1417ec32c541e6fc1bf8
William To <towilliam03@gmail.com>
2020-03-08 14:03:31 -0400

    Add linking to item page for item card

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -12,7 +12,7 @@ class Item extends React.Component {
   }
   handleClick(props) {
 
-    this.setState({ url:props });
+    this.setState({ url: props });
 
   }
   render() {
@@ -28,7 +28,7 @@ class Item extends React.Component {
     return (
       <div className="Item">
         <div className="Item-Photo">
-            <img src={this.state.url}></img>
+          <img src={this.state.url}></img>
 
         </div>
         <div className="Item-Photo-Nav">{imageList}</div>
@@ -38,10 +38,10 @@ class Item extends React.Component {
           by william
           <br></br>
           <p>Hell of echo park street art intelligentsia, heirloom hella sartorial listicle keytar humblebrag twee. Kitsch sustainable distillery affogato, humblebrag gastropub green juice organic. Vice asymmetrical etsy distillery migas quinoa polaroid. Slow-carb craft beer pour-over pok pok williamsburg sriracha migas fanny pack poutine tilde twee.</p>
-          <LikeButton className='LikeButton'/> 90
+          <LikeButton className='LikeButton' /> 90
           <div className="Item-Detail-Button">
-              <Button>Contact User</Button>
-             
+            <Button>Contact User</Button>
+
           </div>
         </div>
       </div>
