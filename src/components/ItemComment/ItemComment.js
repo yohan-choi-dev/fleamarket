c96e423e7fd576f765d9effb7dc136ec1ed6ff95
wingtunglau <wtlau@mysenca.ca>
2020-03-11 13:32:24 -0400

    item comment

---
+++ src/components/ItemComment/ItemComment.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import '../../vars/style.css';
+import './ItemComment.css';
+import { ReactComponent as StarIcon } from '@fortawesome/fontawesome-free/svgs/solid/star.svg';
+import TextField from '../TextField/TextField';
+import Button from '../Button/Button';
+
+class Item extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const comments = this.props.comment;
+    const list = comments.map(c => {
+      return (
+        <div className="ItemComment-Detail">
+          <div className="ItemComment-Detail-User">
+            <img src={c.user}></img> <b>{c.name}</b>
+            <StarIcon className="StarIcon" />
+            <p>{c.rate}</p>
+          </div>
+          <div className="ItemComment-Detail-Content">
+            <p>{c.content}</p>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <div className="ItemComment">
+        <div className="ItemComment-Detail-Container">{list}</div>
+        <div className='ItemComment-Reply'>
+            <TextField text='Write your comment...' size='120'/> <Button>Comment</Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Item;
