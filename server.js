5d46a51c76b7cb90722a0c36278720d1c6d7a483
William To <towilliam03@gmail.com>
2020-03-12 14:42:29 -0400

    Marge with master

--- server.js
+++ server.js
@@ -13,117 +13,119 @@ if (cluster.isMaster) {
         console.log(`worker ${worker.process.pid} died`);
     });
 } else {
-        const express = require("express");
+    const express = require("express");
 
-        const path = require("path");
-        const fs = require("fs");
-        const bodyParser = require("body-parser");
-        const multer = require('multer');
-        const cryptoRandomString = require('crypto-random-string');
-        const cors = require('cors');
+    const path = require("path");
+    const fs = require("fs");
+    const bodyParser = require("body-parser");
+    const multer = require('multer');
+    const cryptoRandomString = require('crypto-random-string');
+    const cors = require('cors');
 
-        // import a database and models
-        const sequelize = require("./utils/database");
-        const User = require("./models/user");
-        const Item = require("./models/item");
-        const ImageLink = require('./models/image-link');
-        const UserItemBridge = require("./models/user-item-bridge");
-        const Category = require("./models/category");
-        const ItemCategoryBridge = require("./models/item-category-bridge");
-        const Comment = require("./models/comment");
-        const Token = require("./models/token");
-        const Feedback = require("./models/feedback");
-        const Message = require("./models/message");
-        const Notification = require("./models/notification");
-        const Trade = require("./models/trade");
+    // import a database and models
+    const sequelize = require("./utils/database");
+    const User = require("./models/user");
+    const Item = require("./models/item");
+    const ImageLink = require('./models/image-link');
+    const UserItemBridge = require("./models/user-item-bridge");
+    const Category = require("./models/category");
+    const ItemCategoryBridge = require("./models/item-category-bridge");
+    const Comment = require("./models/comment");
+    const Token = require("./models/token");
+    const Feedback = require("./models/feedback");
+    const Message = require("./models/message");
+    const Notification = require("./models/notification");
+    const Trade = require("./models/trade");
 
-        const port = 12218;
+    const port = 12218;
 
-        const authRoutes = require("./routes/auth");
-        const itemRoutes = require("./routes/items");
-        const categoryRoutes = require("./routes/category");
+    const authRoutes = require("./routes/auth");
+    const itemRoutes = require("./routes/items");
+    const categoryRoutes = require("./routes/category");
 
-        const ChatService = require("./service/chat-service");
-        const app = express();
+    const ChatService = require("./service/chat-service");
+    const app = express();
 
-        const corsOptions = {
-            origin: 'http://localhost:3000',
-            optionsSuccessStatus: 200
-        }
+    const corsOptions = {
+        origin: 'http://localhost:3000',
+        optionsSuccessStatus: 200
+    }
 
-        app.use(cors(corsOptions));
+    app.use(cors(corsOptions));
 
-        app.use((req, res, next) => {
-            res.setHeader("Access-Control-Allow-Origin", "*");
+    app.use((req, res, next) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
 
-            res.setHeader(
-                "Access-Control-Allow-Methods",
-                "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-            );
-
-            res.setHeader(
-                "Access-Control-Allow-Headers",
-                "Content-Type, Authorization, mutilpart/form-data, applcation/json"
-            );
-            if (req.method == "OPTIONS") {
-                res.status(200).send();
-            }
-            next();
-        });
-
-
-        const fileStorage = multer.diskStorage({
-            destination: (req, res, cb) => {
-                cb(null, "images");
-            },
-            filename: (req, file, cb) => {
-                cb(null, cryptoRandomString({length: 20, type: 'base64'}) + path.extname(file.originalname));
-            }
-        });
-
-        const fileFilter = (req, file, cb) => {
-            if (
-                file.mimetype == "image/png" ||
-                file.mimetype == "image/jpg" ||
-                file.mimetype == "image/jpeg"
-            ) {
-                cb(null, true);
-            }
-            {
-                cb(null, false);
-            }
-        };
-
-        app.use(bodyParser.json());
-        
-        app.use(
-            multer({ storage: fileStorage, fileFilter: fileFilter }).single(
-                "image"
-            )
+        res.setHeader(
+            "Access-Control-Allow-Methods",
+            "GET, POST, PUT, PATCH, DELETE, OPTIONS"
         );
 
-        app.use("/images", express.static(path.join(__dirname, "images")));
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "Content-Type, Authorization, mutilpart/form-data, applcation/json"
+        );
+        if (req.method == "OPTIONS") {
+            res.status(200).send();
+        }
+        next();
+    });
 
-        app.use("/api/auth", authRoutes);
-        app.use("/api/items", itemRoutes);
-        app.use("/api/categories", categoryRoutes);
 
-        app.use((error, req, res, next) => {
-            const status = error.statusCode || 500;
-            const message = error.message;
-            const data = error.data;
-            res.status(status).json({ message: message, data: data });
-        });
+    const fileStorage = multer.diskStorage({
+        destination: (req, res, cb) => {
+            cb(null, "images");
+        },
+        filename: (req, file, cb) => {
+            cb(null, cryptoRandomString({ length: 20, type: 'base64' }) + path.extname(file.originalname));
+        }
+    });
 
-        sequelize
-            .sync()
-            .then(res => {
-                const server = app.listen(port, () =>
-                    console.log(`Worker ${process.pid} is running on ${port}`)
-                );
+    const fileFilter = (req, file, cb) => {
+        if (
+            file.mimetype == "image/png" ||
+            file.mimetype == "image/jpg" ||
+            file.mimetype == "image/jpeg"
+        ) {
+            cb(null, true);
+        }
+        {
+            cb(null, false);
+        }
+    };
+
+    app.use(bodyParser.json());
+
+    app.use(
+        multer({ storage: fileStorage, fileFilter: fileFilter }).single(
+            "image"
+        )
+    );
+
+    app.use("/images", express.static(path.join(__dirname, "images")));
+
+    app.use("/api/auth", authRoutes);
+    app.use("/api/items", itemRoutes);
+    app.use("/api/categories", categoryRoutes);
+
+    app.use((error, req, res, next) => {
+        const status = error.statusCode || 500;
+        const message = error.message;
+        const data = error.data;
+        res.status(status).json({ message: message, data: data });
+    });
+
+    sequelize
+        .sync()
+        .then(res => {
+            const server = app.listen(port, () =>
+                console.log(`Worker ${process.pid} is running on ${port}`)
+            );
+            if (process.env.NODE_ENV == 'production') {
                 const mailService = require("./service/mail-service").init();
-            })
-            .catch(err => {
-                console.log(err);
-            });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
