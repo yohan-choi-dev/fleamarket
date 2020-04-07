95a84f852f3e1338138c4f74c9190d0946533dbe
Yohan Choi <Yohan Choi>
2020-04-07 00:31:09 -0400

    test status 1

--- client/io-client-trade-a.js
+++ client/io-client-trade-a.js
@@ -1,7 +1,26 @@
+/*
+Pseudo Code
+
+James is in the chatroom with Alex
+James chooses an item to exchange
+James clicks Trade with Alex button
+Alex clicks confirm trade button
+Alex clicks cancels trade button
+James clicks complete trade button
+
+Once Trade has been completed, then a user can give start
+
+*/
+
 function User(id, name, email) {
     this.id = id
     this.name = name
     this.email = email
+}
+
+function Item(id, name) {
+    this.id = id
+    this.name = name
 }
 
 function Socket(io, url, user) {
@@ -13,24 +32,81 @@ function Socket(io, url, user) {
     return this.io
 }
 
-function initTrading(user) {
-    const input = process.stdin
-    input.setEncoding('utf-8')
-    input.on('data', (message) => {})
+function requestTrade(socket, user, item) {
+    socket.emit('user.request.trade', user, item)
 }
 
-function joinTrading() {}
+function acceptReqeustTrade(socket, user) {
+    socket.emit('user.request.accepted', user)
+}
+
+function confirmRequest(socket, user) {
+    socket.emit('user.request.confirm', user)
+}
+
+function confirmTrade(socket, user) {
+    socket.emit('user.confirm.trade', user)
+}
+
+function cancelTrade(socket, itemA, itemB) {
+    socket.emit('user.cancel.trade', itemA, itemB)
+}
 
 const io = require('socket.io-client')
 const ROOT_URL = 'http://localhost:12218'
 const TRADE_URL = 'http://localhost:12218/trade'
 
 // init user info
-const userA = new User(7, 'james.bourne', 'james.bourne@myseneca.ca')
-const userB = new User(8, 'alex.smith', 'alex.smith@myseneca.ca')
+const userA = new User(71, 'William To', 'towilliam03@gmail.com')
+const itemA = new Item(198, 'Vans oldskool')
+
+const userB = new User(72, 'Yohan Choi', 'ychoi63@myseneca.ca')
+const itemB = new Item(200, 'Apple is best! Buy Apple!')
 
 // init socket
 const socket = new Socket(io, ROOT_URL, userA)
 const trade = new Socket(io, TRADE_URL, userA)
+
+trade.on('user.request.trade.done')
+
+trade.on('user.request.trade.accepted.done')
+
+trade.on('user.request.confirm.sent')
+
+trade.on('user.confirm.trade.done')
+
+trade.on('user.cancel.trade.done')
+
+const initTrading = (user) => {
+    const input = process.stdin
+    input.setEncoding('utf-8')
+    input.on('data', (message) => {
+        switch (message.trim()) {
+            case 'requestTrade':
+                console.log('requestTrade')
+                requestTrade(trade, userA, itemA)
+                break
+            case 'acceptRequestTrade':
+                console.log('acceptRequestTrade')
+                acceptReqeustTrade(trade, userA)
+                break
+            case 'confirmRequest':
+                console.log('confirmRequest')
+                confirmRequest(trade, userA)
+                break
+            case 'confirmTrade':
+                console.log('confirmTrade')
+                confirmTrade(trade, userA)
+                break
+            case 'cancelTrade':
+                console.log('cancelTrade')
+                cancelTrade(trade, itemA, itemB)
+                break
+            default:
+                console.log('default')
+                break
+        }
+    })
+}
 
 initTrading(userA)
--- events/trading-event.js
+++ events/trading-event.js
@@ -1,22 +1,10 @@
 const crytpoRandomString = require('crypto-random-string')
-const UserItemBridge = require('../model/UserItemBridge')
+const UserItemBridge = require('../models/user-item-bridge')
 const Trade = require('../models/token')
-/*
- Pseudo Code
 
-James is in the chatroom with Alex
-James chooses an item to exchange
-James clicks Trade with Alex button
-Alex clicks confirm trade button
-Alex clicks cancels trade button
-James clicks complete trade button
-
-Once Trade has been completed, then a user can give start
-
-*/
 module.exports = (io) => {
-    const trading = io.of('/trading')
-    trading.on('connection', (socket) => {
+    const trade = io.of('/trade')
+    trade.on('connection', (socket) => {
         try {
             socket.join(socket.handshake.query.id)
             console.log(socket.handshake.query.id)
@@ -26,10 +14,12 @@ module.exports = (io) => {
         }
 
         socket.on('user.request.trade', (user, item) => {
-            socket.to(user.id).broadcast.emit('user.request.trade.done', { user: user, item: item })
+            console.log('user.request.trade')
+            trade.to(user.id).broadcast.emit('user.request.trade.done', { user: user, item: item })
         })
 
         socket.on('user.request.trade.accepted', async (user) => {
+            console.log('user.request.trade.accepted')
             try {
                 const result = await Trade.create({
                     token: crytpoRandomString({ length: 20 }),
@@ -46,10 +36,12 @@ module.exports = (io) => {
         // this will be used when a user clicks a confirm button
         //  so the user is waiting for another user's response.
         socket.on('user.request.confirm', (user) => {
-            io.to(socket.handshake.query.id).broadcast(user).emit('user.request.confirm.sent')
+            console.log('user.request.confirm')
+            trade.to(socket.handshake.query.id).broadcast(user).emit('user.request.confirm.sent')
         })
 
         socket.on('user.confirm.trade', async (itemA, itemB) => {
+            console.log('user.confirm.trade')
             try {
                 const result1 = await UserItemBridge.update(
                     { userId: itemA.userId },
@@ -67,20 +59,21 @@ module.exports = (io) => {
                         }
                     }
                 )
-                io.to(itemA.userId).emit('user.confirm.trade.done', [result1, result2])
+                trade.to(itemA.userId).emit('user.confirm.trade.done', [result1, result2])
             } catch (err) {
                 socket.emit('error', err)
                 console.error(err)
             }
         })
         socket.on('user.cancel.trade', async (tradeId) => {
+            console.log('user.cancel.trade')
             try {
                 const result = await Trade.delete({
                     where: {
                         id: tradeId
                     }
                 })
-                io.to(socket.handshake.query.id).emit('user.cancel.trade.done', result)
+                trade.to(socket.handshake.query.id).emit('user.cancel.trade.done', result)
             } catch (err) {
                 socket.emit('error', err)
                 console.error(err)
